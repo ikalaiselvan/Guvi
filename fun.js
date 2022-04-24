d5f45e3966e890eebfcf4b91726e6ac0333cfb51
ikalaiselvan <104163661+ikalaiselvan@users.noreply.github.com>
2022-04-24 07:38:38 +0530

    Class-4

---
+++ fun.js
@@ -0,0 +1,55 @@
+// let name= "Kalai"; //declaration line
+// let name= "selvan"; //we can't re declare
+// console.log(name);
+
+// let name1= "Kalai";
+// name1= "selvi"; // We can re assign
+// console.log(name1);
+
+// var name = "Kalai";
+// var name = "Selvan"; // We can re declare
+// console.log(name);
+
+// var name1 = "Kalai";
+// name1 = "selvi"; // We can re assign
+// console.log(name);
+
+
+// const name3= "Kalai";
+// const name3= "Selvan"; //We cant re declare
+// console.log(name3);
+
+// const name3 = "Kalai";
+// name3 = "Selvi"; // We cant re assign
+// consol.log(name3)
+
+
+// let kalai;
+// console.log(kalai) // result is Undefined
+
+// var good;
+// console.log(typeof good) // result also undefined
+
+// Block scope "{ }" let cant escape, var can escape
+// function scope "function driving() { }" let, var both are can't escape
+
+// function drivingTest(age) {
+//   if (age > 18) {
+//     var msg = "eligible";
+//   } else {
+//     var msg = "not eligible";
+//   }
+//   console.log("💂🏽‍♀️ are " + msg);
+// }
+// drivingTest(20);
+
+// // Type casting / Implicit coercion (automatically convert datatype by JS)
+// var value1 = 2;
+// var value2 = "5";
+// console.log(value1+ value2) //Result is '25' (Its happen Concatenatiion) 
+
+// // Explicit coercion (we are forcing to convert datatype)
+// console.log(value1+ parseInt(value2))   //Result is '7'
+// console.log(value1 + +value2)           //Result is '7'
+// console.log(value1 + Number(value2))    //Result is '7'
+
